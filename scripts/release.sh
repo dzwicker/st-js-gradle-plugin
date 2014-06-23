a04dc13a04e21b5c0483dad1216d8e1b0235f173
dzwicker <daniel.zwicker@in2experience.com>
2014-06-23 16:20:48 +0200

    use release plugin

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,6 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 base_dir=${script_dir/\/scripts/}
 
 echo "Base dir: $base_dir"
-#export JAVA_HOME="$(/usr/libexec/java_home -v 1.7*)"
 (cd ${base_dir}
-    ./gradlew clean check assemble uploadArchives
+    ./gradlew clean test release
 )
